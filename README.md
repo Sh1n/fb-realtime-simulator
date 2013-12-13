f23a08de51591ec852a4b8f015bbe54697d16d24
shin <shin@fucinaludica.it>
2013-12-13 10:12:59 +0100

    Better documentation

--- README.md
+++ README.md
@@ -6,4 +6,14 @@ Simulator for messages coming from Facebook Realtime Updates
 Usage
 =====
 
-ruby fbtester.rb
+```ruby
+require 'FacebookSimulator'
+simulator = FacebookSimulator.new
+simulator.verifyToken = YOUR_VERIFY_TOKEN
+simulator.callbackUrl = YOUR_CALLBACK_URL
+sim.simulateRealtimeUpdate(subscriptionPayload)
+```
+where subscriptionPayload is the payload is an hash resembling the Facebook subscription object. The method will send the JSON version.
+
+
+
--- fbtester.rb
+++ fbtester.rb
@@ -25,42 +25,64 @@ VERIFY_TOKEN = "TEST-BEANCOUNTER-FACEBOOK"
 # =================================================================
 # Callback Url: requests will be sent to this address
 # =================================================================
-CALLBACK_URL = "http://0.0.0.0:34567/facebook"
+CALLBACK_URL = "http://192.168.10.10:34567/facebook"
 
 # =============================================
 # Hash of the payload sent to the user
 # =============================================
 payload = {:object => "user", :entry => [{:uid => userId, :id => userId, :time => Time.now.to_f, :changed_fields => ["likes"]}]}
 
-puts "Facebook RealTime Update Tester"
-puts payload.to_json
+class FacebookSimulator
+	
+	attr_accessor :verifyToken, :callbackUrl, :appSecret, :appId, :appSecret, :debug  
+	attr_reader :challenge
 
-#
-# Verification
-#
-hub = {
-	"verify_token" => VERIFY_TOKEN,
-	"mode" => "subscribe",
-	"challenge" => SecureRandom.hex()
-}
-puts "Sending Verification" 
-puts hub
-begin
-	response = RestClient.get CALLBACK_URL, hub
-	if response.code == 200 && response.to_str == hub[:challenge]
-		puts "Verification Ok"
-		puts "Sending Stream"
-		# Streaming
-		response = RestClient.put CALLBACK_URL,  payload.to_json, :content_type => :json
-		puts "Streaming: [" + response.code.to_str + "] " + response.to_str
-	else
-		puts "Received " + response.code.to_str
-		puts response.to_str
+	def simulateRealtimeUpdate(payload)
+		@challenge = SecureRandom.hex()
+		hub = {
+			:params => {
+				"hub.verify_token" => @verifyToken,
+				"hub.mode" => "subscribe",
+				"hub.challenge" => @challenge
+			}
+		}
+		if @debug
+			puts "Hub Payload: " + hub.to_s
+		end
+		begin
+			if @debug
+				puts "Sending Verification to: " + @callbackUrl.to_s
+			end
+			response = RestClient.get @callbackUrl, hub
+			if response.code == 200 and response == @challenge
+				if @debug
+					puts "Verification response: [" + response.code + "] " + response
+					puts "Sending Stream: " + payload.to_s
+				end
+				# Streaming
+				response = RestClient.put @callbackUrl,  payload.to_json, :content_type => :json
+				if @debug
+					puts "Received: [code." + response.code + "] " + response
+				end
+				return true
+			else
+				if @debug
+					puts "Received: [code." + response.code + "] " + response
+				end
+				return false
+			end
+		rescue Exception => e  
+			puts e.message  
+		  	puts e.backtrace.inspect
+		end
 	end
-rescue
-	puts "Address not reachable"
 end
-	# Send GET -> hub.verify_token = $, hub.mode -> "subscribe" -> expects $(hub.challenge)
 
-
-# Streaming
+puts "==========================="
+puts "    Facebook Simulator"
+puts "==========================="
+sim = FacebookSimulator.new
+sim.debug = true
+sim.verifyToken = VERIFY_TOKEN
+sim.callbackUrl = CALLBACK_URL
+sim.simulateRealtimeUpdate(payload)
